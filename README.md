73849445178b0c0a39be85c7be5f57441e0f1d60
Ramon Silveira Borges <ramonboorges@gmail.com>
2023-10-18 20:41:42 +0200

    feat: added routes to list and register transactions + filter per user session using cookie

---
+++ README.md
@@ -0,0 +1,10 @@
+Requisitos Funcionais(RF)
+- [X] O usuário deve poder criar uma conta;
+- [X] O usuário deve poder obter um extrato da sua conta;
+- [X] O usuário deve poder listar todas transações que já ocorreram;
+- [X] O usuário deve poder visualizar uma transação única.
+
+Regras de Negócios(RN)
+- [X] A transação pode do tipo crédito que somará ao valor total, ou débito que será subtraído;
+- [X] Deve ser possível identificar o usuário entre as requisições;
+- [X] O usuário só pode visualizar transações que ele criou.
--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,13 @@
 import fastify from 'fastify'
-import { knex } from './database'
-import crypto from 'node:crypto'
+import cookie from '@fastify/cookie'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.post('/insert-transaction', async () => {
-  const transaction = await knex('transactions')
-    .insert({
-      id: crypto.randomUUID(),
-      title: 'Transação de teste',
-      amount: 1000,
-    })
-    .returning('*')
-
-  return transaction
-})
-
-app.get('/get-transactions', async () => {
-  const transactions = await knex('transactions')
-    .where('amount', 1000)
-    .select('*')
-
-  return transactions
+app.register(cookie)
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
 })
 
 app
